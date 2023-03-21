3db9d513c2a78df0ca9cbef49420a01990e556fd
Brook Slagle <brook.slagle@trojans.dsu.edu>
2023-03-20 22:46:07 -0500

    reee

--- src/authCodeWithPkce.ts
+++ src/authCodeWithPkce.ts
@@ -22,7 +22,7 @@ export async function getAccessToken(clientId: string, code: string) {
     params.append("client_id", clientId);
     params.append("grant_type", "authorization_code");
     params.append("code", code);
-    params.append("redirect_uri", "hhttps://aquamarine-pasca-1f7e3a.netlify.app/callback");
+    params.append("redirect_uri", "https://aquamarine-pasca-1f7e3a.netlify.app/callback");
     params.append("code_verifier", verifier!);
 
     const result = await fetch("https://accounts.spotify.com/api/token", {
