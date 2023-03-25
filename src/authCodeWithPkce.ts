4fded66249213a231b33050ad38631d53f40a151
Brook Slagle <brook.slagle@trojans.dsu.edu>
2023-03-24 23:02:40 -0500

    merge with dev - add search and styling

--- src/authCodeWithPkce.ts
+++ src/authCodeWithPkce.ts
@@ -11,7 +11,7 @@ export async function redirectToAuthCodeFlow(clientId: string) {
     const params = new URLSearchParams();
     params.append("client_id", clientId);
     params.append("response_type", "code");
-    params.append("redirect_uri", "http://localhost:5173/callback");
+    params.append("redirect_uri", "https://writer-for-playlist-club.netlify.app/callback");
     params.append("scope", "user-read-private user-read-email");
     params.append("code_challenge_method", "S256");
     params.append("code_challenge", challenge);
@@ -30,7 +30,7 @@ export async function getAccessToken(clientId: string, code: string) {
     params.append("client_id", clientId);
     params.append("grant_type", "authorization_code");
     params.append("code", code);
-    params.append("redirect_uri", "http://localhost:5173/callback");
+    params.append("redirect_uri", "https://writer-for-playlist-club.netlify.app/callback");
     params.append("code_verifier", verifier!);
 
     console.log('getaccesstoken', params)
